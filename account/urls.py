bcfdb0015f5ee2ed81d01d316f359b918ab07d41
meraj1234 <merajhossainfec@gmail.com>
2023-12-22 20:59:22 +0600

    Initial commit

---
+++ account/urls.py
@@ -0,0 +1,8 @@
+
+from django.urls import path,include
+from account.views import UserRegistrationView,UserLoginView,UserProfileView
+urlpatterns = [
+    path('register/',UserRegistrationView.as_view(),name='register'),
+    path('login/',UserLoginView.as_view(),name='login'),
+    path('userprofile/',UserProfileView.as_view(),name='login')
+]
