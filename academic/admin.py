add4fcf3d65c6b32fa9297cecf3f926cc9eab79d
Ishak Sheikh <ishaksheikh174363@gmail.com>
2023-12-26 10:55:27 +0600

    Merge branch ishak into main

--- academic/admin.py
+++ academic/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from .models import subject,exam,notice,result,club
+from .models import subject,exam,notice,result,club,Class
 
-class SubjectAdmin(admin.ModelAdmin):
-    list_display = ('subject_id', 'subject_name', 'subject_code', 'teacher_id')
-
-admin.site.register(subject, SubjectAdmin)
+admin.site.register(subject)    
 admin.site.register(exam)
 admin.site.register(notice)
 admin.site.register(result)
 admin.site.register(club)
+admin.site.register(Class)
 
