28902b1072a46e22c29cdf827a3b0a89f0d376a6
meraj1234 <merajhossainfec@gmail.com>
2023-12-26 15:41:33 +0600

    Exam and Result api created

--- academic/urls.py
+++ academic/urls.py
@@ -1,6 +1,7 @@
 # urls.py
 from django.urls import path
-from .views import NoticeListCreateView, NoticeRetrieveUpdateDeleteView,SubjectCreateView, SubjectUpdateView, SubjectDeleteView
+from .views import NoticeListCreateView, NoticeRetrieveUpdateDeleteView,SubjectCreateView, SubjectUpdateView, SubjectDeleteView,ExamCreateView, ExamUpdateView, ExamDeleteView,ResultRetrieveView, ResultCreateView, ResultUpdateView, ResultDeleteView
+
 
 urlpatterns = [
     #any one will perform to see notice
@@ -8,8 +9,24 @@ urlpatterns = [
     #only head are able to perform notice create,update and delete
     path('notices/<int:pk>/', NoticeRetrieveUpdateDeleteView.as_view(), name='notice-retrieve-update-delete'),
     
-    #Onnly head will be able to perform subject create,update and delete
+    #Only head will be able to perform subject create,update and delete
     path('subjects/create/', SubjectCreateView.as_view(), name='subject-create'),
     path('subjects/update/<int:subject_id>/', SubjectUpdateView.as_view(), name='subject-update'),
     path('subjects/delete/<int:subject_id>/', SubjectDeleteView.as_view(), name='subject-delete'),
+    
+    #Only head will be able to perform exam create,update and delete
+    path('exam/create/', ExamCreateView.as_view(), name='exam-create'),
+    path('exam/update/<int:pk>/', ExamUpdateView.as_view(), name='exam-update'),
+    path('exam/delete/<int:pk>/', ExamDeleteView.as_view(), name='exam-delete'),
+    
+    
+    ###Only Head and Teacher are able to perform result create update and delete 
+    
+    path('result/create/', ResultCreateView.as_view(), name='result-create'),
+    path('result/update/<int:pk>/', ResultUpdateView.as_view(), name='result-update'),
+    path('result/delete/<int:pk>/', ResultDeleteView.as_view(), name='result-delete'),
+    
+    
+    ## ###Any one can be view or retrieve result
+    path('result/<int:pk>/', ResultRetrieveView.as_view(), name='result-retrieve'),
 ]
--- academic/views.py
+++ academic/views.py
@@ -1,8 +1,10 @@
 from rest_framework import generics, permissions
-from .models import notice, subject
-from .serializers import NoticeSerializer,SubjectSerializer
-from .permissions import IsHeadUser
+from .models import notice, subject,exam,result
+from .serializers import NoticeSerializer,SubjectSerializer,ExamSerializer,ResultSerializer
+from .permissions import IsHeadUser,IsHeadAndTeacherUser
 
+
+# notice part
 class NoticeListCreateView(generics.ListCreateAPIView):
     queryset = notice.objects.all()
     serializer_class = NoticeSerializer
@@ -16,8 +18,9 @@ class NoticeRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = notice.objects.all()
     serializer_class = NoticeSerializer
     permission_classes = [IsHeadUser]
-    
-
+   
+ 
+##subjects part
 class SubjectCreateView(generics.CreateAPIView):
     queryset = subject.objects.all()
     serializer_class = SubjectSerializer
@@ -34,4 +37,57 @@ class SubjectDeleteView(generics.DestroyAPIView):
     serializer_class = SubjectSerializer
     permission_classes = [IsHeadUser]
     lookup_field = 'subject_id'
+    
+    
+## exam part
+
+class ExamCreateView(generics.CreateAPIView):
+    queryset = exam.objects.all()
+    serializer_class = ExamSerializer
+    permission_classes = [IsHeadUser]
+
+class ExamUpdateView(generics.UpdateAPIView):
+    queryset = exam.objects.all()
+    serializer_class = ExamSerializer
+    permission_classes = [IsHeadUser]
+
+class ExamDeleteView(generics.DestroyAPIView):
+    queryset = exam.objects.all()
+    serializer_class = ExamSerializer
+    permission_classes = [IsHeadUser]
+    
+    
+    
+##Result part
+
+
+    
+
+class ResultCreateView(generics.CreateAPIView):
+    queryset = result.objects.all()
+    serializer_class = ResultSerializer
+    permission_classes = [IsHeadAndTeacherUser]
+    
+
+class ResultUpdateView(generics.UpdateAPIView):
+    queryset = result.objects.all()
+    serializer_class = ResultSerializer
+    permission_classes = [IsHeadAndTeacherUser]
+    
+
+class ResultDeleteView(generics.DestroyAPIView):
+    queryset = result.objects.all()
+    serializer_class = ResultSerializer
+    permission_classes = [IsHeadAndTeacherUser]
+    
+
+class ResultRetrieveView(generics.RetrieveAPIView):
+    queryset = result.objects.all()
+    serializer_class = ResultSerializer
+    permission_classes = [IsHeadAndTeacherUser]
+    
+
+
+
+
 
